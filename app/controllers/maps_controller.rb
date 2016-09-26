22e5f75d5b644e5a79fd4fb0fd8c4d06e6be8325
satub <satu.barnhill@gmail.com>
2016-09-26 16:39:45 -0400

    Added create-action plus #no_locations? and #assign_locations to map controller and models

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,11 +5,15 @@ class MapsController < ApplicationController
   end
 
   def create
-    binding.pry
-    ##create locations:
+    # binding.pry
+    @map = Map.create(map_params)
+    ###MAP NEEDS A VALIDATION AGAINST LETTERS AND EMPTY VALUES IN THE SIZE BOXES!!!
+    @map.assign_locations if @map.no_locations?
+    redirect_to map_path(@map)
   end
 
   def show
+    @map = Map.find(params[:id])
   end
 
   private
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,17 @@
 class Map < ApplicationRecord
   belongs_to :game
   has_many :locations
+
+
+  def no_locations?
+    self.locations.empty?
+  end
+
+  def assign_locations
+    number_of_locations = self.size_x.to_i * self.size_y.to_i
+    number_of_locations.times do
+      location = Location.create(map_id: self.id, content: Faker::ChuckNorris.fact)
+    end
+  end
+
 end
