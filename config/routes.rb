20385404e5470fefd6e4bd8d6228f0af280e8b12
satub <satu.barnhill@gmail.com>
2016-09-26 12:08:43 -0400

    basic routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :players, :controllers =>  {:registrations => 'players/registrations', :omniauth_callbacks => "players/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#home"
+  get '/about' => "welcome#about"
 
+  resources :teams do
+    resources :characters
+  end
+  resources :games
+
+  resources :maps do
+    resources :locations
+  end
 end
