693c3f8e17def5faea9267c10c9009efadc490a1
satub <satu.barnhill@gmail.com>
2016-10-02 19:49:21 -0400

    randomized defense during assigning locations

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,12 +7,7 @@ class Game < ApplicationRecord
 
   validates :title, presence: true
   validates :map_size, numericality: {only_integer: true}, inclusion: {in: (4..20)}
-
-  ###Custom functions:
-  ###no more than (2) characters per player per game (would be nicer if this was dependent of map_size)
-  ###be able to tell when a game is over
-  ###be able to shut off joining in function after 2 players in a game
-
+  
   def self.pending_games
     Game.where(status: "pending").order(:id)
   end
@@ -58,12 +53,12 @@ class Game < ApplicationRecord
   def assign_locations(player_array)  ##works only for 2-players atm
     if player_array.size == 1
       self.map_size.times do
-        location = Location.create(game_id: self.id, controlled_by: player_array[0].id, content: Faker::ChuckNorris.fact, defense: 100)
+        location = Location.create(game_id: self.id, controlled_by: player_array[0].id, content: Faker::ChuckNorris.fact, defense: rand(50..200))
       end
     else
       i = 0
       until i >= self.map_size/2
-        self.locations[i].update(controlled_by: player_array[1].id, content: Faker::StarWars.quote, defense: 100)
+        self.locations[i].update(controlled_by: player_array[1].id, content: Faker::StarWars.quote, defense: rand(50..200))
         i += 1
       end
     end
