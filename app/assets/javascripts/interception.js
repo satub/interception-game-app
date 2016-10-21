2a1f870597a1d356e529d1232fdcc89596e8e507
satub <satu.barnhill@gmail.com>
2016-10-21 12:27:21 -0400

    navbar done with flex, added custom font

--- app/assets/javascripts/interception.js
+++ app/assets/javascripts/interception.js
@@ -2,42 +2,42 @@
 
 
 function addMenuListeners(){
-  $("#navbar a:contains('Home')").bind("click", function(event){
+  $("a:contains('Home')").bind("click", function(event){
     defaultStopper(event);
     cleanScreen();
     console.log("Definitely Maybe You're My Wonderwall")
   });
 
-  $("#navbar a:contains('About')").bind("click", function(event){
+  $("a:contains('About')").bind("click", function(event){
     defaultStopper(event);
     console.log("Here we will print the rules! (Not Vanderpump)");
     printRules();
   });
 
-  $('#navbar a[href="/games"]').bind("click", function(event){
+  $('a[href="/games"]').bind("click", function(event){
     defaultStopper(event);
     console.log("Fetch ALL the games, my demon doggy!")
     fetchGames();
   });
 
-  $('#navbar a[href="/games/new"]').bind("click", function(event){
+  $('a[href="/games/new"]').bind("click", function(event){
     defaultStopper(event);
     generateNewForm("games");
   });
 
   if (loggedIn()){
-    $('#navbar a[href="/players/' + playerId + '/characters/new"]').bind("click", function(event){
+    $('a[href="/players/' + playerId + '/characters/new"]').bind("click", function(event){
       defaultStopper(event);
       generateNewForm('players/' + playerId + '/characters');
     });
   }
 
-  $("#navbar a:contains('My Characters')").bind("click", function(event){
+  $("a:contains('My Characters')").bind("click", function(event){
     defaultStopper(event);
     fetchMyCharacters();
   });
 
-  $("#shortcut a:contains('Current game')").bind("click", function(event){
+  $("a:contains('Current game')").bind("click", function(event){
     defaultStopper(event);
     fetchGameViaUrl(event.currentTarget.href);
   });
